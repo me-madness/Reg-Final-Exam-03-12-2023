203ac98e250b322c21f97806c4e8a1a43936d727
me_madness <p_rangel@abv.bg>
2023-12-03 14:43:34 +0100

    Replace work well

--- 01. decrypting_commands.py
+++ 01. decrypting_commands.py
@@ -1,18 +1,27 @@
 message = input()
-command = input()
+command = input().split(' ')
 
-while command != "Finish":
-    command = input().split(' ')
-    if command == "Replace":
+while command[0] != "Finish":
+    if command[0] == "Replace":
         current_char = command[1]
         new_char = command[2]
         new_message = message.replace(current_char, new_char)
         print(f"{new_message}")
-    elif command == "Cut":
+    elif command[0] == "Cut":
         pass
-    elif command == "Make":
+    elif command[0] == "Make":
         pass
-    elif command == "Check":
+    elif command[0] == "Check":
         pass
-    elif command == "Sum":
+    elif command[0] == "Sum":
         pass
+    
+    
+# ILikeSoftUni
+# Replace I We
+# Make Upper
+# Check SoftUni
+# Sum 1 4
+# Cut 1 4
+# Finish
+
